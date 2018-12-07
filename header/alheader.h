3cd199f371b19bc0a49dfa6a41e8e9b438a29eca
kdyeon <doyeun96429@gmail.com>
2018-12-06 23:42:15 -0800

    header directtory

---
+++ header/alheader.h
@@ -0,0 +1,12 @@
+#ifdef ALHEADER_H
+#define ALHEADER_H
+
+void solveSudoku(int _r, int _c);
+void initReadOnlySudoku(void);
+int isOKUD(int _r, int _c, int _value);
+int isOKLR(int _r, int _c, int _value);
+int isOKregion(int _r, int _c, int _value);
+void printSolution(void);
+void checkNext(int _r, int _c);
+
+#endif
