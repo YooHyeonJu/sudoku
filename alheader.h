486ac03d19a818196e73834386e5c189d75f9da9
kdyeon <doyeun96429@gmail.com>
2018-12-06 23:27:28 -0800

    algorithm header

--- alheader.h
+++ alheader.h
@@ -4,7 +4,9 @@
 void solveSudoku(int _r, int _c);
 int readOnlySudoku[SZ][SZ];
 void initReadOnlySudoku(void);
-int isOK(int _r, int _c, int _value);
+int isOKUD(int _r, int _c, int _value);
+int isOKLR(int _r, int _c, int _value);
+int isOKregion(int _r, int _c, int _value);
 void printSolution(void);
 void checkNext(int _r, int _c);
 
